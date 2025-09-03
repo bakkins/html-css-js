af6e9f4a5da4484fe096c081f74d52b1dadb2266
unknown <llaurisb07@gmail.com>
2025-09-03 11:41:01 +0300

    made changes and added table tennis

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("Your Name");
+
+function printNumbers() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+window.onload = printNumbers;
+
+const button = document.getElementById("button");
+button.addEventListener("click", printNumbers);
