31e2cc3770eecbac57e3564a796f9c5493230198
Lauris <llaurisb07@gmail.com>
2025-09-04 11:36:57 +0300

    daudz kas

--- app.js
+++ app.js
@@ -6,7 +6,30 @@ function printNumbers() {
     }
 }
 
-window.onload = printNumbers;
+function logName() {
+    console.log("Lauris");
+}
 
-const button = document.getElementById("button");
-button.addEventListener("click", printNumbers);
+function confirmDisable(event) {
+    event.preventDefault();
+
+    const formName = document.getElementById("name").value;
+    const formEmail = document.getElementById("email").value;
+    const formFeedback = document.getElementById("feedback").value;
+
+    console.log(formName, "\n", formEmail, "\n", formFeedback);
+}
+
+// const button = document.getElementById("button");
+// button.addEventListener("click", printNumbers);
+
+// const pageTitle = document.getElementById("nameTitle");
+// pageTitle.addEventListener("click", logName);
+
+// const firstParagraph = document.querySelector("#paragraph");
+// firstParagraph.parentNode.removeChild(firstParagraph);
+
+document.addEventListener('DOMContentLoaded', () => {
+const form = document.getElementById('form');
+form.addEventListener("submit", confirmDisable);
+});
